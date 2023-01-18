0da68f117f1c44e69a6cd932a0d34ae39dcaa00f
junior <juniormathiias@gmail.com>
2023-01-17 21:49:56 -0300

    just finished the project with some changes of hiddens and conditionals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import { db } from './firebaseConnection';
-import './app.css'
 import { useState, useEffect } from 'react';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot  } from 'firebase/firestore';
+import { db, auth } from './firebaseConnection';
+import './app.css'
+import { 
+  doc, 
+  setDoc, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  updateDoc, 
+  deleteDoc,
+  onSnapshot
+} from 'firebase/firestore';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
   const [idPost, setIdPost] = useState('');
   const [post, setPosts] = useState([]);
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const [user, setUser] = useState(false);
+  const [userDetail, setUserDetail] = useState({});
+
+
   useEffect(() => {
     async function loadPosts(){
       const unsub = onSnapshot(collection(db, "posts"), (snapshot) => {
@@ -24,6 +48,24 @@ function App() {
     }
     loadPosts();
   }, []);
+  
+  useEffect(() => {
+    async function checkLogin(){
+      onAuthStateChanged(auth, (user) => {
+        if(user){
+          setUser(true);
+          setUserDetail({
+            uid: user.uid,
+          email: user.email,
+          })
+        }else{
+          setUser(false);
+          setUserDetail({});
+        }
+      })
+    }
+    checkLogin();
+  },[])
 
   async function handleAdd(){
     // await setDoc(doc(db, "posts", "12345"), {
@@ -103,9 +145,85 @@ function App() {
       alert("Erro ao Deletar" + error)
     })
   }
+  async function novoUsuario(){
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then(() => {
+      alert("CADASTRADO COM SUCESSO!")
+    
+      setEmail('');
+      setSenha('');
+    })
+    .catch((error) => {
+      
+      if(error.code === 'auth/weak-password'){
+        alert("Senha muito fraca.")
+      }else if(error.code === 'auth/email-already-in-use'){
+        alert("Email já existe!")
+      }
+
+    })
+  }
+  async function logarUsuario(){
+    await signInWithEmailAndPassword(auth, email, senha)
+    .then((value) => {
+      alert("logado");
+      setUserDetail({
+        uid: value.user.uid,
+        email: value.user.email,
+      })
+
+      setUser(true)
+
+      setEmail('')
+      setSenha('')
+    })
+    .catch((error) => {
+      alert(error)
+    })
+  }
+  async function fazerLogout(){
+    await signOut(auth)
+    setUser(false);
+    setUserDetail({});
+  }
+
   return (
     <div>
-      <h2>hello world</h2>
+      
+      { user && (
+        <div> 
+          <strong>seja bem-vindo você está logado</strong><br/>
+          <span>ID: {userDetail.uid} - Email: {userDetail.email}</span>
+          <br/><br/>
+          <button onClick={fazerLogout}> Sair</button>
+          <br/><br/>
+        </div>
+      )}
+
+      <div className="container">
+      <h2>Usuarios</h2>
+
+      <label>Email</label>
+      <input 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)} 
+        placeholder="Digite um email"
+      /> <br/>
+
+      <label>Senha</label>
+      <input 
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)} 
+        placeholder="Informe sua senha"
+      /> <br/> 
+
+      <button onClick={novoUsuario}>Cadastrar</button> 
+      <button onClick={logarUsuario}>Fazer Login</button>     
+    </div>
+
+    <br/><br/>
+    <hr/>
+    <h2>Posts</h2>
       <div className='container'>
       <label>ID do Post:</label><br/>
       <input
